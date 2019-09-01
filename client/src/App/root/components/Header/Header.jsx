b2ff4f3b4801eaa2e8ce9a89b511b8259ffa708c
ptomasz <ptomasz1772@gmail.com>
2019-09-01 13:08:00 +0200

    Header first version

--- client/src/App/root/components/Header/Header.jsx
+++ client/src/App/root/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import { FaSearch, FaVideo } from "react-icons/fa";
+import styles from "./Header.module.css";
 
 function Header(props) {
-  return <div>Header</div>;
+  return (
+    <Navbar color="dark" dark>
+      <NavbarBrand className={styles.brand} href="/">
+        <FaVideo className={styles.icon} /> Movie application
+      </NavbarBrand>
+      <Nav className={styles.items} navbar>
+        <NavItem>
+          <NavLink href="/">Main Page</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  );
 }
 
 export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import RootProvider from "./App/root/context/RootProvider";
 import Root from "./App/root/Root";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <RootProvider>
