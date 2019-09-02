e5431d77cfeb753ae5b4edaf0e2d5d16f7dbe5d6
ptomasz <ptomasz1772@gmail.com>
2019-09-02 02:03:40 +0200

    DetailsPage final version

--- client/src/App/root/components/Header/Header.jsx
+++ client/src/App/root/components/Header/Header.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 import { FaVideo } from "react-icons/fa";
 import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
     <Navbar color="dark" dark>
-      <NavbarBrand className={styles.brand} href="/">
-        <FaVideo className={styles.icon} /> Movie application
+      <NavbarBrand className={styles.brand}>
+        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+          <FaVideo className={styles.icon} /> Movie application
+        </Link>
       </NavbarBrand>
       <Nav className={styles.items} navbar>
         <NavItem>
-          <NavLink href="/">Main Page</NavLink>
+          <NavLink>
+            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+              Main Page
+            </Link>
+          </NavLink>
         </NavItem>
       </Nav>
     </Navbar>
--- client/src/App/root/components/MovieElement/MovieElement.jsx
+++ client/src/App/root/components/MovieElement/MovieElement.jsx
@@ -7,35 +7,36 @@ function MovieElement(props) {
   const { data, id } = props;
 
   return (
-    <Jumbotron className={styles.movieElement}>
-      <Row>
-        <Col xs="5">
-          <img src={data.poster} alt={data.title} />
-        </Col>
-        <Col xs="5">
-          <h2>
-            {data.title}, {data.date}
-          </h2>
-          <div className={styles.details}>
-            {data.director === "N/A" ? null : data.director + ", "}
-            {data.country === "N/A" ? null : data.country + ", "}
-            {data.type === "N/A" ? null : data.type + ", "}
-          </div>
-          <h4 className={styles.plot}>
-            {data.plot === "N/A" ? null : data.plot}
-          </h4>
-        </Col>
-        <Col xs="2">
-          <h2 className={styles.rating}>{data.rating} / 10</h2>
-          <Link
-            to={`/movie/${id}`}
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
+    <Link
+      to={`/movie/${id}`}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <Jumbotron className={styles.movieElement}>
+        <Row>
+          <Col xs="5">
+            <img src={data.poster} alt={data.title} />
+          </Col>
+          <Col xs="5">
+            <h2>
+              {data.title}, {data.date}
+            </h2>
+            <div className={styles.details}>
+              {data.director === "N/A" ? null : data.director + ", "}
+              {data.country === "N/A" ? null : data.country + ", "}
+              {data.type === "N/A" ? null : data.type + ", "}
+            </div>
+            <h4 className={styles.plot}>
+              {data.plot === "N/A" ? null : data.plot}
+            </h4>
+          </Col>
+          <Col xs="2">
+            <h2 className={styles.rating}>{data.rating} / 10</h2>
+
             <Button className={styles.button}>See more</Button>
-          </Link>
-        </Col>
-      </Row>
-    </Jumbotron>
+          </Col>
+        </Row>
+      </Jumbotron>
+    </Link>
   );
 }
 
